c53b399b910d9ba1c30b94fd47a47823d9417c5c
Luis Arturo García González <lgarcia103@alumnos.uaq.mx>
2021-11-20 18:36:33 -0600

    feat: Index update

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,36 +1,81 @@
 <style>
-  :global(body.dark) {
-    --background: #0C2D48;
-    --foreground: #D4E7F4;
-    --secondary-foreground: #FFFFFF;
+  :global(.top-lg) {
+    margin-top: 2rem;
+  }
+  :global(.top-md) {
+    margin-top: 1rem;
+  }
+  :global(.top-sm) {
+    margin-top: 0.5rem;
+  }
+  :global(.bottom-lg) {
+    margin-bottom: 2rem;
+  }
+  :global(.bottom-md) {
+    margin-bottom: 1rem;
+  }
+  :global(.bottom-sm) {
+    margin-bottom: 0.5rem;
   }
 
-  :global(body.light) {
-    --background: #D4E7F4;
-    --foreground: #0C2D48;
-    --secondary-foreground: #161616;
-  }
-
-  :global(*) {
-    margin: 0;
-    padding: 0;
-  }
-
-  :global(body, html){
+  #main {
+    box-sizing: border-box;
     width: 100%;
-    height: 100%;
+    height: 100vh;
+    padding: 8rem 3rem 0 3rem;
   }
 
-  :global(body) {
-    font-family: 'Poppins', sans-serif;
-    background-color: var(--background);
-    color: var(--foreground);
+  #text-container {
+    width: 45rem;
+  }
 
-    --accent: #19BD91;
-    --danger: #EE544A;
+  .title {
+    font-size: 2.8rem;
+  }
+  
+  .subtitle {
+    font-size: 1.2rem;
+    line-height: 2rem;
+  }
+
+  .text {
+    font-size: 1rem;
+  }
+
+  .button {
+    padding: .5rem 1.5rem;
+    border-radius: 5px;
+    text-decoration: none;
+    color: var(--white);
+    font-weight: bold;
+    font-size: 1.1rem;
+    box-sizing: border-box;
+  }
+
+  .green {
+    background-color: var(--accent);
+  }
+
+  .main-image {
+    position: absolute;
+    font-size: 38vw;
+    color: var(--accent);
+    right: -5rem;
+    bottom: -2rem;
   }
 </style>
 
-<div>
-  <h1>Hola mundo</h1>
-</div>
+<section id="main">
+  <div id="text-container">
+    <h1 class="title">Representación de aplicación para relación de conjuntos difusos</h1>
+    <p class="subtitle top-md">Contestando una serie de preguntas sobre tus síntomas sería posible determinar en cierto grado si se tiene o no una relación sintomática con una o varias enfermedades</p>
+    <p class="text top-lg">*Es un proyecto escolar con propósitos demostrativos únicamente</p>
+  </div>
+
+  <div class="top-lg">
+    <a class="button green" href="/prueba" sveltekit:prefetch>Tomar test</a>
+  </div>
+
+  <i class="fab fa-medrt main-image" />
+  <i class="fab fa-medrt main-image" />
+</section>
