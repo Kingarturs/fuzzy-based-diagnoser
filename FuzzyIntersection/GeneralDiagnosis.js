32011cb3e09a0f00c0e84f6b583f4238198fcfd5
Raúl <53493295+rguzg@users.noreply.github.com>
2021-11-21 19:54:27 -0600

    Merge branch 'main' into dev

---
+++ FuzzyIntersection/GeneralDiagnosis.js
@@ -0,0 +1,75 @@
+import variables from './variables.js'
+
+
+
+
+
+export function InterseccionDifusa(ArrayUsuario,EnfermedadesSelected){
+//Num Sintomas 
+var Sintomas = 17;
+
+//Array con datos del Usuario : Cambiar A datos Finales
+var ArrayUser =ArrayUsuario
+
+//Seleccion de Enfermedades a Evaluar
+var EnfermedadesSeleccionadas= EnfermedadesSelected
+
+//Array Datos Finales
+var ArrayIntersecciones = []
+
+//Enfermedades desde JSON
+var enfermedades = variables.data
+
+var Porcientaje = []
+var maximo = 0;
+//Calculo de Intersecciones con Valores del Usuario
+var Interseccion = 0;
+enfermedades.forEach((enfermedad,index) => {
+    if (EnfermedadesSeleccionadas[index]==0) {
+        ArrayIntersecciones.push(Interseccion);
+        return;
+    }
+    for (let index = 0; index < Sintomas; index++) {
+        var MinimoConjunto = Math.min(enfermedad.Array[index],ArrayUser[index])
+        Interseccion+= MinimoConjunto
+        maximo += enfermedad.Array[index]
+    }
+    Porcientaje.push((Interseccion/maximo)*100);
+    ArrayIntersecciones.push(Interseccion);
+    Interseccion=0;
+    maximo=0;
+    
+});
+
+
+//Determinación de Interseccion mayor
+var mayor = 0;
+var indexM = 0;
+ArrayIntersecciones.forEach((element,index) => {
+    if(element>mayor){
+        indexM = index;
+        mayor = element;
+    }
+});
+
+if(Porcientaje[indexM]<75){
+    
+    var Respuesta = ["No existe Coincidencia","Coincidencias Menores al 75"]        
+    return Respuesta
+}
+
+var Respuesta = [enfermedades[indexM].Nombre,Porcientaje[indexM]]
+return Respuesta
+
+}
+
+// if(mayor<2){
+//     console.log("Sin Iguales")
+// }
+// console.log(enfermedades[indexM].Nombre);
+// console.log("Confiabilidad == "+Porcientaje[indexM])
+// console.log(ArrayIntersecciones)
+
+
+// console.log("/////")
+// console.log(Porcientaje)
